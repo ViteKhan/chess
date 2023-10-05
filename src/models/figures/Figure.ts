9b1771039d17469f1ea671553cea5306cb3cfa79
ViteKhan <victory_96@mail.ru>
2023-10-05 16:52:15 +0300

    added base class Figure

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,28 @@
-export class Figure {
+import { COLORS, FIGURES } from 'types';
+import FigureLogo from 'assets/images/black-king.png';
+import { Cell } from '../Cell';
 
+export class Figure {
+  color: COLORS;
+  logo: typeof FigureLogo | null;
+  cell: Cell;
+  name: FIGURES;
+  id: number;
+
+  constructor(color: COLORS, cell: Cell) {
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.logo = null;
+    this.name = FIGURES.FIGURE;
+    this.id = Math.random();
+  }
+
+  canMove = (target: Cell): boolean => {
+    return true;
+  };
+
+  moveFigure = (target: Cell) => {
+
+  };
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,13 @@ export enum COLORS {
   WHITE = 'white',
   BLACK = 'black',
 }
+
+export enum FIGURES {
+  FIGURE = 'figure',
+  PAWN = 'pawn',
+  ROOK = 'rook',
+  BISHOP = 'bishop',
+  KNIGHT = 'knight',
+  QUEEN = 'queen',
+  KING = 'king',
+}
