30109fdd3a7ccc99a648916716979060192e4338
ViteKhan <victory_96@mail.ru>
2023-10-05 17:32:55 +0300

    added figure logo to cell

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -9,6 +9,7 @@ interface CellComponentProps {
 export const CellComponent: FC<CellComponentProps> = ({ cell }) => {
   return (
     <div className={['cell', cell.color].join(' ')}>
+      {cell.figure?.logo && <img src={cell.figure.logo} alt={cell.figure.name} title={cell.figure.name} />}
     </div>
   );
 };
