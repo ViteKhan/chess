b0c2ffb5d255a665ca0e94c8947144ce2dd8479d
ViteKhan <victory_96@mail.ru>
2023-10-06 10:21:15 +0300

    added ability to move Rook

--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -16,6 +16,14 @@ export class Rook extends Figure {
       return false;
     }
 
-    return true;
+    if (this.cell.isEmptyVertical(target)) {
+      return true;
+    }
+
+    if (this.cell.isEmptyHorizontal(target)) {
+      return true;
+    }
+
+    return false;
   }
 }
