224de7d31b1464030a166e3fd238fc207cca9e33
ViteKhan <victory_96@mail.ru>
2023-10-06 12:58:44 +0300

    added ability to move Pawn

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -5,6 +5,8 @@ import { Cell } from '../Cell';
 import { Figure } from './Figure';
 
 export class Pawn extends Figure {
+  isFirstStep: boolean = true;
+
   constructor(color: COLORS, cell: Cell) {
     super(color, cell);
     this.logo = color === COLORS.BLACK ? blackPawn : whitePawn;
@@ -16,6 +18,30 @@ export class Pawn extends Figure {
       return false;
     }
 
-    return true;
+    const direction = this.cell.figure?.color === COLORS.BLACK ? 1 : -1;
+    const firstStepDirection = this.cell.figure?.color === COLORS.BLACK ? 2 : -2;
+
+    const isXDirection = target.x === this.cell.x;
+    const isEmptyCellForMove = this.cell.board.getCell(target.x, target.y).IsEmptyCell();
+    const isOffsetByOne = target.y === this.cell.y + direction;
+    const isOffsetByTwo = this.isFirstStep && (target.y === this.cell.y + firstStepDirection);
+
+    if ((isOffsetByOne || isOffsetByTwo) && isXDirection && isEmptyCellForMove) {
+      return true;
+    }
+
+    const isDiagonalUp = target.y === this.cell.y + direction;
+    const isDiagonalUBottom = target.x === this.cell.x + 1 || target.x === this.cell.x - 1;
+
+    if(isDiagonalUp && isDiagonalUBottom && this.cell.isEnemy(target)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  moveFigure(target: Cell) {
+    super.moveFigure(target);
+    this.isFirstStep = false;
   }
 }
